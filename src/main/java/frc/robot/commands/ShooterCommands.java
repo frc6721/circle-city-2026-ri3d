42abda4b1f33e53fb257303eb3460c37ef6c208d
rhit-collinkn <collinkn@rose-hulman.edu>
2026-01-11 19:18:29 -0500

    adding shooter commands

---
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.shooter.Shooter;
+
+public class ShooterCommands {
+
+  public ShooterCommands() {}
+
+  public Command setFlywheelTargetSpeed(Shooter shooter, AngularVelocity speed) {
+    return Commands.run(
+        () -> {
+          shooter.setFlywheelSpeed(speed);
+        },
+        shooter);
+  }
+
+  public Command stopFlywheels(Shooter shooter) {
+    return Commands.run(
+        () -> {
+          shooter.stopFlywheels();
+        },
+        shooter);
+  }
+
+}
