6099843d121566ffd280ed31276ff47d5d2e946c
Garrett Hart <garrett7621@gmail.com>
2026-01-11 21:08:41 -0500

    add shoot for distance logic

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.shooter.Shooter;
@@ -21,6 +22,23 @@ public class ShooterCommands {
     return Commands.run(
         () -> {
           shooter.stopFlywheels();
+        },
+        shooter);
+  }
+
+  /**
+   * Sets the flywheel speed based on distance to target using the lookup table.
+   * The speed is automatically calculated from ShooterConstants interpolation.
+   * 
+   * @param shooter The shooter subsystem
+   * @param distance Distance to the target
+   * @return Command that continuously sets the flywheel speed
+   */
+  public Command setFlywheelSpeedForDistance(Shooter shooter, Distance distance) {
+    return Commands.run(
+        () -> {
+          AngularVelocity targetSpeed = shooter.getSpeedForDistance(distance);
+          shooter.setFlywheelSpeed(targetSpeed);
         },
         shooter);
   }
