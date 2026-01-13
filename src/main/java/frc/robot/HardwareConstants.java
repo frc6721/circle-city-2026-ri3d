7e2c33c9f2caa42d9fe58a54b9aee8f7e55c732e
rhit-collinkn <collinkn@rose-hulman.edu>
2026-01-12 22:36:57 -0500

    update CAN IDs

--- src/main/java/frc/robot/HardwareConstants.java
+++ src/main/java/frc/robot/HardwareConstants.java
@@ -5,12 +5,13 @@ public class HardwareConstants {
 
   public class CanIds {
     // |====================== INTAKE SUBSYSTEM CAN IDs ======================|
-    public static int PIVOT_MOTOR_ID = 21;
-    public static int ROLLER_MOTOR_ID = 20;
+    public static int RIGHT_PIVOT_MOTOR_ID = 20;
+    public static int LEFT_PIVOT_MOTOR_ID = 21;
+    public static int ROLLER_MOTOR_ID = 22;
 
     // |====================== SHOOTER SUBSYSTEM CAN IDs ======================|
     public static int FLYWHEEL_MOTOR_ID = 31;
-    public static int FEEDER_MOTOR_ID = 33;
+    public static int FEEDER_MOTOR_ID = 32;
 
     // |====================== CLIMBER SUBSYSTEM CAN IDs ======================|
     public static int CLIMBER_MOTOR_ID = 42;
