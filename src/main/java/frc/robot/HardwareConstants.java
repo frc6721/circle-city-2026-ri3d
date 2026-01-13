f2802c65f65337824ee594c24b26165b0d95484e
Garrett Hart <garrett7621@gmail.com>
2026-01-13 10:36:13 -0500

    update constants

--- src/main/java/frc/robot/HardwareConstants.java
+++ src/main/java/frc/robot/HardwareConstants.java
@@ -3,20 +3,19 @@ package frc.robot;
 public class HardwareConstants {
   public static final int NUMBER_OF_CAMERAS = 2;
 
-  public class CanIds {
+  public static class CanIds {
     // |====================== INTAKE SUBSYSTEM CAN IDs ======================|
-    public static int PIVOT_MOTOR_ID = 21;
+    public static int RIGHT_PIVOT_MOTOR_ID = 20;
+    public static int LEFT_PIVOT_MOTOR_ID = 21;
     public static int ROLLER_MOTOR_ID = 20;
 
     // |====================== SHOOTER SUBSYSTEM CAN IDs ======================|
     public static int FLYWHEEL_MOTOR_ID = 31;
+
+    // |====================== FEEDER SUBSYSTEM CAN IDs ======================|
     public static int FEEDER_MOTOR_ID = 33;
 
     // |====================== CLIMBER SUBSYSTEM CAN IDs ======================|
     public static int CLIMBER_MOTOR_ID = 42;
-  }
-
-  public class DIOPorts {
-    public static int INTAKE_UPPER_LIMIT_SWITCH_PORT = 0;
   }
 }
