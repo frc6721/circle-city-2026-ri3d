c95d858c4d041c3d8597f703ade7c97515df2185
Garrett Hart <garrett7621@gmail.com>
2026-01-13 09:51:52 -0500

    update canids

--- src/main/java/frc/robot/HardwareConstants.java
+++ src/main/java/frc/robot/HardwareConstants.java
@@ -5,13 +5,12 @@ public class HardwareConstants {
 
   public class CanIds {
     // |====================== INTAKE SUBSYSTEM CAN IDs ======================|
-    public static int RIGHT_PIVOT_MOTOR_ID = 20;
-    public static int LEFT_PIVOT_MOTOR_ID = 21;
-    public static int ROLLER_MOTOR_ID = 22;
+    public static int PIVOT_MOTOR_ID = 21;
+    public static int ROLLER_MOTOR_ID = 20;
 
     // |====================== SHOOTER SUBSYSTEM CAN IDs ======================|
     public static int FLYWHEEL_MOTOR_ID = 31;
-    public static int FEEDER_MOTOR_ID = 32;
+    public static int FEEDER_MOTOR_ID = 33;
 
     // |====================== CLIMBER SUBSYSTEM CAN IDs ======================|
     public static int CLIMBER_MOTOR_ID = 42;
